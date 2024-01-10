eb7a254f46e37a9253cb1d0b45c022030020a3b1
harutomatusako <91893425+harutomatusako@users.noreply.github.com>
2024-01-10 23:54:03 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,3 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Rails.application.configure do
-  config.hosts << "rails-production-6d55.up.railway.app"
-end
