3c76062dfd30b0b498096ad4a565660b0955edbe
harutomatusako <gzpfkbgs.softbank.jp@gmail.com>
2024-01-11 23:58:37 +0900

    update

--- db/migrate/20231214141045_create_table.rb
+++ db/migrate/20231214141045_create_table.rb
@@ -1,4 +1,4 @@
-class CreateEventUsers < ActiveRecord::Migration[7.0]
+class CreateTable < ActiveRecord::Migration[7.0]
   def change
     create_table :event_users do |t|
       t.bigint :users_id, null: false
